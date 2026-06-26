d1b7eca168e4400de5a5302aa9e8e3a26d070287
qimei <qimei>
2026-06-26 15:36:01 +0800

    前端大改：接入demo视觉与交互；新增CC标准收藏榜、A24票房榜、红心收藏；左栏改手风琴+展开/选中分离；奖项交集改AND显示双奖tag；海报按池锁比例对齐

---
+++ data/a24_list.js
@@ -0,0 +1,124 @@
+// A24 年度票房榜 维度数据 (10部，2026年6月真实全球票房)
+// 票房剧变：后室(20岁导演处女作)十天登顶，瞬息全宇宙跌至第3
+const A24_DATA = [
+ {
+  "id": "1083381",
+  "rank": 1,
+  "title_cn": "后室",
+  "title_en": "Backrooms",
+  "year": 2026,
+  "director": "凯恩·帕森斯",
+  "box": "2.12亿美元",
+  "box_raw": 212,
+  "is_new": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/1083381.jpg"
+ },
+ {
+  "id": "1221562",
+  "rank": 2,
+  "title_cn": "至尊马蒂",
+  "title_en": "Marty Supreme",
+  "year": 2025,
+  "director": "本尼·萨夫迪",
+  "box": "1.91亿美元",
+  "box_raw": 191,
+  "is_new": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/1221562.jpg"
+ },
+ {
+  "id": "545611",
+  "rank": 3,
+  "title_cn": "瞬息全宇宙",
+  "title_en": "Everything Everywhere All at Once",
+  "year": 2022,
+  "director": "关家永·丹尼尔·施纳特",
+  "box": "1.47亿美元",
+  "box_raw": 147,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/545611.jpg"
+ },
+ {
+  "id": "1332410",
+  "rank": 4,
+  "title_cn": "爱情抓马",
+  "title_en": "The Drama",
+  "year": 2026,
+  "director": "克里斯托弗·博格利",
+  "box": "1.28亿美元",
+  "box_raw": 128,
+  "is_new": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/1332410.jpg"
+ },
+ {
+  "id": "929590",
+  "rank": 5,
+  "title_cn": "美国内战",
+  "title_en": "Civil War",
+  "year": 2024,
+  "director": "亚历克斯·加兰",
+  "box": "1.27亿美元",
+  "box_raw": 127,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/929590.jpg"
+ },
+ {
+  "id": "1146200",
+  "rank": 6,
+  "title_cn": "物质主义者",
+  "title_en": "Materialists",
+  "year": 2025,
+  "director": "席琳·宋",
+  "box": "1.07亿美元",
+  "box_raw": 107,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/1146200.jpg"
+ },
+ {
+  "id": "1008042",
+  "rank": 7,
+  "title_cn": "回应我",
+  "title_en": "Talk to Me",
+  "year": 2022,
+  "director": "丹尼·菲利普·迈克尔·菲利普",
+  "box": "9200万美元",
+  "box_raw": 92,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/1008042.jpg"
+ },
+ {
+  "id": "493922",
+  "rank": 8,
+  "title_cn": "遗传厄运",
+  "title_en": "Hereditary",
+  "year": 2018,
+  "director": "阿里·艾斯特",
+  "box": "9000万美元",
+  "box_raw": 90,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/493922.jpg"
+ },
+ {
+  "id": "391713",
+  "rank": 9,
+  "title_cn": "伯德小姐",
+  "title_en": "Lady Bird",
+  "year": 2017,
+  "director": "格蕾塔·葛韦格",
+  "box": "7900万美元",
+  "box_raw": 79,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/391713.jpg"
+ },
+ {
+  "id": "376867",
+  "rank": 10,
+  "title_cn": "月光男孩",
+  "title_en": "Moonlight",
+  "year": 2016,
+  "director": "巴里·杰金斯",
+  "box": "6600万美元",
+  "box_raw": 66,
+  "is_new": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/posters/376867.jpg"
+ }
+];
---
+++ data/cc_list.js
@@ -0,0 +1,504 @@
+// CC 标准收藏·畅销榜 维度数据 (50: 44单片+6套装)
+// v6: 新增 director 字段
+const CC_DATA = [
+ {
+  "id": "1243",
+  "spine": 1243,
+  "title_cn": "老无所依",
+  "director": "科恩兄弟",
+  "year": 2007,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1243.jpg",
+  "cross": false
+ },
+ {
+  "id": "1254",
+  "spine": 1254,
+  "title_cn": "哥斯拉VS碧奥兰蒂",
+  "director": "大森一树",
+  "year": 1989,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1254.jpg",
+  "cross": false
+ },
+ {
+  "id": "12",
+  "spine": 12,
+  "title_cn": "摇滚万万岁",
+  "director": "罗伯·莱纳",
+  "year": 1984,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/12.jpg",
+  "cross": false
+ },
+ {
+  "id": "691",
+  "spine": 691,
+  "title_cn": "大盗独行",
+  "director": "迈克尔·曼",
+  "year": 1981,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/691.jpg",
+  "cross": false
+ },
+ {
+  "id": "1259",
+  "spine": 1259,
+  "title_cn": "阿诺拉",
+  "director": "肖恩·贝克",
+  "year": 2024,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1259.jpg",
+  "cross": true
+ },
+ {
+  "id": "1283",
+  "spine": 1283,
+  "title_cn": "暴力史",
+  "director": "大卫·柯南伯格",
+  "year": 2005,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1283.jpg",
+  "cross": false
+ },
+ {
+  "id": "1278",
+  "spine": 1278,
+  "title_cn": "Flow",
+  "director": "金茨·兹巴洛迪斯",
+  "year": 2024,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1278.jpg",
+  "cross": false
+ },
+ {
+  "id": "1267",
+  "spine": 1267,
+  "title_cn": "千惊万险",
+  "director": "威廉·弗里德金",
+  "year": 1977,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1267.jpg",
+  "cross": false
+ },
+ {
+  "id": "501",
+  "spine": 501,
+  "title_cn": "德州巴黎",
+  "director": "维姆·文德斯",
+  "year": 1984,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/501.jpg",
+  "cross": false
+ },
+ {
+  "id": "977",
+  "spine": 977,
+  "title_cn": "蓝丝绒",
+  "director": "大卫·林奇",
+  "year": 1986,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/977.jpg",
+  "cross": false
+ },
+ {
+  "id": "2",
+  "spine": 2,
+  "title_cn": "七武士",
+  "director": "黑泽明",
+  "year": 1954,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/2.jpg",
+  "cross": false
+ },
+ {
+  "id": "779",
+  "spine": 779,
+  "title_cn": "穆赫兰道",
+  "director": "大卫·林奇",
+  "year": 2001,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/779.jpg",
+  "cross": false
+ },
+ {
+  "id": "1284",
+  "spine": 1284,
+  "title_cn": "灵魂大搜索",
+  "director": "肯·罗素",
+  "year": 1980,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1284.jpg",
+  "cross": false
+ },
+ {
+  "id": "843",
+  "spine": 843,
+  "title_cn": "私恋失调",
+  "director": "保罗·托马斯·安德森",
+  "year": 2002,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/843.jpg",
+  "cross": false
+ },
+ {
+  "id": "897",
+  "spine": 897,
+  "title_cn": "巴里·林登",
+  "director": "斯坦利·库布里克",
+  "year": 1975,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/897.jpg",
+  "cross": false
+ },
+ {
+  "id": "898",
+  "spine": 898,
+  "title_cn": "双峰：与火同行",
+  "director": "大卫·林奇",
+  "year": 1992,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/898.jpg",
+  "cross": false
+ },
+ {
+  "id": "36",
+  "spine": 36,
+  "title_cn": "恐惧的代价",
+  "director": "亨利-乔治·克鲁佐",
+  "year": 1953,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/36.jpg",
+  "cross": false
+ },
+ {
+  "id": "24",
+  "spine": 24,
+  "title_cn": "天国与地狱",
+  "director": "黑泽明",
+  "year": 1963,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/24.jpg",
+  "cross": false
+ },
+ {
+  "id": "1152",
+  "spine": 1152,
+  "title_cn": "妖夜慌踪",
+  "director": "大卫·林奇",
+  "year": 1997,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1152.jpg",
+  "cross": false
+ },
+ {
+  "id": "100",
+  "spine": 100,
+  "title_cn": "野兽男孩演出录像精选",
+  "director": "多位",
+  "year": 1982,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/100.jpg",
+  "cross": false
+ },
+ {
+  "id": "948",
+  "spine": 948,
+  "title_cn": "公主新娘",
+  "director": "罗伯·莱纳",
+  "year": 1987,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/948.jpg",
+  "cross": false
+ },
+ {
+  "id": "1248",
+  "spine": 1248,
+  "title_cn": "百战宝枪",
+  "director": "安东尼·曼",
+  "year": 1950,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1248.jpg",
+  "cross": false
+ },
+ {
+  "id": "1161",
+  "spine": 1161,
+  "title_cn": "机器人总动员",
+  "director": "安德鲁·斯坦顿",
+  "year": 2008,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1161.jpg",
+  "cross": false
+ },
+ {
+  "id": "13",
+  "spine": 13,
+  "title_cn": "沉默的羔羊",
+  "director": "乔纳森·戴米",
+  "year": 1991,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/13.jpg",
+  "cross": false
+ },
+ {
+  "id": "1025",
+  "spine": 1025,
+  "title_cn": "布达佩斯大饭店",
+  "director": "韦斯·安德森",
+  "year": 2014,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1025.jpg",
+  "cross": true
+ },
+ {
+  "id": "1054",
+  "spine": 1054,
+  "title_cn": "寄生虫",
+  "director": "奉俊昊",
+  "year": 2019,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1054.jpg",
+  "cross": true
+ },
+ {
+  "id": "539",
+  "spine": 539,
+  "title_cn": "鬼怪屋",
+  "director": "大林宣彦",
+  "year": 1977,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/539.jpg",
+  "cross": false
+ },
+ {
+  "id": "1104",
+  "spine": 1104,
+  "title_cn": "公民凯恩",
+  "director": "奥逊·威尔斯",
+  "year": 1941,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1104.jpg",
+  "cross": false
+ },
+ {
+  "id": "1059",
+  "spine": 1059,
+  "title_cn": "欲望号快车",
+  "director": "大卫·柯南伯格",
+  "year": 1996,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1059.jpg",
+  "cross": false
+ },
+ {
+  "id": "1035",
+  "spine": 1035,
+  "title_cn": "自己去看",
+  "director": "埃列姆·克利莫夫",
+  "year": 1985,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1035.jpg",
+  "cross": false
+ },
+ {
+  "id": "562",
+  "spine": 562,
+  "title_cn": "凶线",
+  "director": "布莱恩·德·帕尔玛",
+  "year": 1981,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/562.jpg",
+  "cross": false
+ },
+ {
+  "id": "137",
+  "spine": 137,
+  "title_cn": "美人计",
+  "director": "阿尔弗雷德·希区柯克",
+  "year": 1946,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/137.jpg",
+  "cross": false
+ },
+ {
+  "id": "942",
+  "spine": 942,
+  "title_cn": "生命之树",
+  "director": "泰伦斯·马力克",
+  "year": 2011,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/942.jpg",
+  "cross": true
+ },
+ {
+  "id": "1134",
+  "spine": 1134,
+  "title_cn": "愤怒的公牛",
+  "director": "马丁·斯科塞斯",
+  "year": 1980,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1134.jpg",
+  "cross": false
+ },
+ {
+  "id": "1101",
+  "spine": 1101,
+  "title_cn": "原钻",
+  "director": "萨夫迪兄弟",
+  "year": 2019,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1101.jpg",
+  "cross": false
+ },
+ {
+  "id": "336",
+  "spine": 336,
+  "title_cn": "年少轻狂",
+  "director": "理查德·林克莱特",
+  "year": 1993,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/336.jpg",
+  "cross": false
+ },
+ {
+  "id": "44",
+  "spine": 44,
+  "title_cn": "红菱艳",
+  "director": "鲍威尔与普雷斯伯格",
+  "year": 1948,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/44.jpg",
+  "cross": false
+ },
+ {
+  "id": "837",
+  "spine": 837,
+  "title_cn": "十诫",
+  "director": "克日什托夫·基耶斯洛夫斯基",
+  "year": 1988,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/837.jpg",
+  "cross": false
+ },
+ {
+  "id": "849",
+  "spine": 849,
+  "title_cn": "女友礼拜五",
+  "director": "霍华德·霍克斯",
+  "year": 1940,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/849.jpg",
+  "cross": false
+ },
+ {
+  "id": "1160",
+  "spine": 1160,
+  "title_cn": "黑潮",
+  "director": "斯派克·李",
+  "year": 1992,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1160.jpg",
+  "cross": false
+ },
+ {
+  "id": "905",
+  "spine": 905,
+  "title_cn": "早餐俱乐部",
+  "director": "约翰·休斯",
+  "year": 1985,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/905.jpg",
+  "cross": false
+ },
+ {
+  "id": "1175",
+  "spine": 1175,
+  "title_cn": "内陆帝国",
+  "director": "大卫·林奇",
+  "year": 2006,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1175.jpg",
+  "cross": false
+ },
+ {
+  "id": "1073",
+  "spine": 1073,
+  "title_cn": "杀人回忆",
+  "director": "奉俊昊",
+  "year": 2003,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1073.jpg",
+  "cross": false
+ },
+ {
+  "id": "1185",
+  "spine": 1185,
+  "title_cn": "下班后",
+  "director": "马丁·斯科塞斯",
+  "year": 1985,
+  "is_boxset": false,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/1185.jpg",
+  "cross": false
+ },
+ {
+  "id": "box01",
+  "spine": 0,
+  "title_cn": "椿三十郎：武士两部曲",
+  "director": "黑泽明",
+  "year": "1961-1962",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box01.jpg",
+  "cross": false
+ },
+ {
+  "id": "box02",
+  "spine": 0,
+  "title_cn": "韦斯安德森收藏",
+  "director": "韦斯·安德森",
+  "year": "1996-2021",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box02.jpg",
+  "cross": false
+ },
+ {
+  "id": "box03",
+  "spine": 0,
+  "title_cn": "李小龙：他的巅峰之作",
+  "director": "多位",
+  "year": "1971-1978",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box03.jpg",
+  "cross": false
+ },
+ {
+  "id": "box04",
+  "spine": 0,
+  "title_cn": "爱在三部曲",
+  "director": "理查德·林克莱特",
+  "year": "1995-2013",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box04.jpg",
+  "cross": false
+ },
+ {
+  "id": "box05",
+  "spine": 0,
+  "title_cn": "王家卫的世界",
+  "director": "王家卫",
+  "year": "1998-2004",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box05.jpg",
+  "cross": false
+ },
+ {
+  "id": "box06",
+  "spine": 0,
+  "title_cn": "约翰·卡萨维蒂：五部曲",
+  "director": "约翰·卡萨维蒂",
+  "year": "1959-2000",
+  "is_boxset": true,
+  "poster": "https://world-movie-selection.oss-cn-shanghai.aliyuncs.com/WMS-cc-bestseller-posters/box06.jpg",
+  "cross": false
+ }
+];
